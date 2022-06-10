e960bea33346284393a9a0d2999ae1053c1c9bef
nthai <nthaitrx@gmail.com>
2022-06-10 12:05:55 +0200

    updated code with stackoverflow example

--- maxflow.cc
+++ maxflow.cc
@@ -1,51 +1,118 @@
-#include <boost/config.hpp>
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <iomanip>
+// based on: https://stackoverflow.com/a/67622091/7925855
 
-#include <boost/graph/edmonds_karp_max_flow.hpp>
 #include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/read_dimacs.hpp>
-#include <boost/graph/graph_utility.hpp>
+#include <boost/graph/boykov_kolmogorov_max_flow.hpp>
+#include <boost/range/adaptors.hpp>
+#include <fmt/ostream.h>
+#include <fmt/ranges.h>
+#include <fstream>
+using boost::adaptors::filtered;
 
-typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> Traits;
-typedef boost::adjacency_list < boost::listS, boost::vecS, boost::directedS,
-    boost::property<boost::vertex_name_t, std::string>,
-    boost::property<boost::edge_capacity_t, double,
-    boost::property<boost::edge_residual_capacity_t, double,
-    boost::property<boost::edge_reverse_t, Traits::edge_descriptor>>>> Graph;
+using Traits = boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS>;
+using V      = Traits::vertex_descriptor;
+using E      = Traits::edge_descriptor;
 
-void print_graph(std::vector<std::vector<double>> const& g) {
-    for (auto const& r : g) {
-        for (auto const& x : r) {
-            std::cout << std::setw(4) << x << " ";
+using Capacity = double;
+using Color    = boost::default_color_type;
+
+struct VertexProps {
+    // std::string name;
+    Color    color;
+    Capacity distance;
+    E        predecessor;
+};
+
+struct EdgeProps {
+    int      id;
+    Capacity weight, residual;
+    E        reverse;
+};
+
+using Graph = boost::adjacency_list<
+    boost::vecS, boost::vecS, boost::directedS,
+    VertexProps,
+    // see https://stackoverflow.com/a/64744086/85371 :(
+    boost::property<boost::edge_capacity_t, Capacity, EdgeProps>>;
+
+struct MyGraph {
+    MyGraph(size_t nnodes) : _g(nnodes) {}
+
+    void runSimulation(auto const& arcs, auto const& capacities, V src, V sink)
+    {
+        reconfigure(arcs, capacities);
+
+        Capacity maxflow = solve_max_flow(src, sink);
+
+        auto cap       = get(boost::edge_capacity, _g);
+        auto is_source = [this](V v) { return _g[v].color == Color::black_color; };
+
+        fmt::print("Max flow {}\nNodes {} are in source subset\n", maxflow,
+                vertices(_g) | filtered(is_source));
+
+        for (E e : boost::make_iterator_range(edges(_g))) {
+            bool st_cut =
+                is_source(source(e, _g)) and
+                not is_source(target(e, _g));
+
+            fmt::print("Edge {} (id #{:2}), capacity {:3} {}\n", e, _g[e].id,
+                    cap[e], st_cut ? "(ST Cut)" : "");
         }
-        std::cout << "\n";
     }
-}
 
-void print_graph(Graph& g) {
-    boost::graph_traits<Graph>::vertex_iterator u_iter, u_end;
-    boost::graph_traits<Graph>::out_edge_iterator ei, e_end;
+private:
+    Graph _g;
 
-    boost::property_map<Graph, boost::edge_capacity_t>::type capacity = boost::get(boost::edge_capacity, g);
-    
-    for (boost::tie(u_iter, u_end) = boost::vertices(g); u_iter != u_end; ++u_iter) {
-        for (boost::tie(ei, e_end) = boost::out_edges(*u_iter, g); ei != e_end; ++ei) {
-            std::cout << *u_iter << " " << boost::target(*ei, g) << " "
-                << capacity[*ei] << std::endl;
+    void reconfigure(auto const& arcs, auto const& capacities)
+    {
+        assert(arcs.size() == capacities.size());
+
+        for (auto v : boost::make_iterator_range(vertices(_g))) {
+            // boost::clear_out_edges(v, g);
+            boost::clear_vertex(v, _g);
+        }
+
+        auto cap  = get(boost::edge_capacity, _g);
+        auto eidx = get(&EdgeProps::id, _g);
+        auto rev  = get(&EdgeProps::reverse, _g);
+
+        auto  eindex = 0;
+
+        for (auto [fr, to] : arcs) {
+            auto edf  = add_edge(fr, to, _g).first;
+            auto edr  = add_edge(to, fr, _g).first;
+            eidx[edf] = 2 * eindex;
+            eidx[edr] = eidx[edf] + 1;
+            cap[edf]  = cap[edr] = capacities[eindex];
+
+            rev[edf] = edr;
+            rev[edr] = edf;
+
+            ++eindex;
         }
     }
-}
+
+    Capacity solve_max_flow(V src, V sink)
+    {
+        return boykov_kolmogorov_max_flow(
+            _g, src, sink,
+            // named arguments
+            boost::reverse_edge_map(get(&EdgeProps::reverse, _g))
+                .residual_capacity_map(get(&EdgeProps::residual, _g))
+                .vertex_color_map(get(&VertexProps::color,       _g))
+                .predecessor_map(get(&VertexProps::predecessor,  _g))
+                .distance_map(get(&VertexProps::distance,        _g))
+            // end named arguments
+        );
+    }
+};
 
 std::vector<std::vector<double>> get_mod_capacities() {
-    std::ifstream infile("graph.dat");
+    using namespace std;
+    ifstream infile("graph.dat");
     int vcount, ecount;
     infile >> vcount >> ecount;
 
-    std::vector<std::vector<double>> graph(vcount + 2, std::vector<double>(vcount + 2, 0));
+    vector<vector<double>> graph(vcount + 2, vector<double>(vcount + 2, 0));
 
     for (int i = 0; i < ecount; i++) {
         int from, to;
@@ -57,37 +124,37 @@ std::vector<std::vector<double>> get_mod_capacities() {
         graph[from + 1][vcount + 1] += lower;
     }
 
-    // print_graph(graph);
-    
     return graph;
 }
 
 int main() {
-    std::vector<std::vector<double>> mod_capacities = get_mod_capacities();
-    unsigned mod_vcount = mod_capacities.size();
+    using namespace std;
 
-    Graph g;
-
-    std::vector<Traits::vertex_descriptor> vdesc(mod_vcount);
-    for (int i = 0; i < mod_vcount; i++) {
-        vdesc[i] = boost::add_vertex(g);
-    }
-
-    for (size_t from = 0; from < mod_vcount; from++) {
-        for (size_t to = 0; to < mod_vcount; to++) {
-            if (mod_capacities[from][to] != 0) {
-                boost::add_edge(from, to, { mod_capacities[from][to], 0.0 }, g);
+    vector<vector<double>> adj_matrix = get_mod_capacities();
+    vector<pair<long, long>> edges;
+    vector<double> caps;
+    for (size_t i = 0; i < adj_matrix.size(); ++i) {
+        for (size_t j = 0; j < adj_matrix[i].size(); j++) {
+            if (adj_matrix[i][j]) {
+                edges.push_back(make_pair(i, j));
+                caps.push_back(adj_matrix[i][j]);
             }
         }
     }
-    // print_graph(g);
 
-    Traits::vertex_descriptor s = vdesc[0], t = vdesc[mod_vcount - 1];
-    boost::property_map<Graph, boost::edge_capacity_t>::type capacity = boost::get(boost::edge_capacity, g);
-    boost::property_map<Graph, boost::edge_reverse_t>::type rev = boost::get(boost::edge_reverse, g);
-    boost::property_map<Graph, boost::edge_residual_capacity_t>::type residual_capacity = boost::get(boost::edge_residual_capacity, g);
-    std::vector<boost::default_color_type> color(boost::num_vertices(g));
-    std::vector<Traits::edge_descriptor> pred(boost::num_vertices(g));
-    double flow = boost::edmonds_karp_max_flow(g, s, t, capacity, residual_capacity, rev, &color[0], &pred[0]);
-    std::cout << flow << std::endl;
+    MyGraph g{ adj_matrix.size() };
+    
+    for (auto&& [arcs, capacities] : { tuple
+            // // 1
+            // {vector{pair{0, 1}, {0, 2}, {1, 2}, {1, 3}, {1, 4},
+            //                         {2, 4}, {3, 4}, {3, 5}, {4, 5}},
+            // vector{10, 10, 10, 10, 1, 4, 3, 2, 10}},
+            // // 2
+            // {vector{pair{0, 1}, {0, 2}, {1, 3}, {2, 4}, {3, 5}, {4, 5}},
+            // vector{10, 10, 10, 4, 2, 0}},
+            {edges, caps}
+        })
+    {
+        g.runSimulation(arcs, capacities, 0, 5);
+    }
 }
